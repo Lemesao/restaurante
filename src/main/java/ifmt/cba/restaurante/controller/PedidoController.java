1efabbdf09e8ec5c8e3a83f4d2be28ba8ecbf4ce
mateus <mateuslfn00@gmail.com>
2025-07-15 20:28:27 -0400

    pedido inicio

---
+++ src/main/java/ifmt/cba/restaurante/controller/PedidoController.java
@@ -0,0 +1,11 @@
+package ifmt.cba.restaurante.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/Pedido")
+public class PedidoController {
+
+}
